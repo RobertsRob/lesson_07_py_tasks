526adf8873ea52766e6e7a32600fc6f424727322
RobertsRob <83211059+RobertsRob@users.noreply.github.com>
2024-09-14 22:30:22 +0300

    Update tasks_1.py

--- tasks_1.py
+++ tasks_1.py
@@ -1,8 +1,21 @@
 # 1.task
+# Создайте кортеж с числами и найдите максимальное и минимальное значение.
+numbers = (3, 8, 2, 10, 6)
+max_value = max(numbers)
+min_value = min(numbers)
+print(max_value, min_value)
 
 # 2.task
+# Создайте множество с числами и добавьте в него новое число.
+numbers_set = {1, 2, 3, 4}
+numbers_set.add(5)
+print(numbers_set)
 
 # 3.task
+# Создайте словарь с именами и возрастами, получите возраст одного человека "безопасно" по его имени.
+people = {"Alice": 25, "Bob": 30, "Charlie": 22}
+age = people.get("Bob")
+print(age)
 
 # 4.task
 
