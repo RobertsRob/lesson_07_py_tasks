396d9eed973a30cef43e01236c6c6933d5c8c6b3
RobertsRob <83211059+RobertsRob@users.noreply.github.com>
2024-09-18 20:04:05 +0300

    Update tasks_2.py

--- tasks_2.py
+++ tasks_2.py
@@ -1,9 +1,25 @@
 # 1.task
+# Используя lambda, сложите два числа.
+sum_two = lambda x, y: x + y
+result = sum_two(5, 7)
+print(result)
 
 # 2.task
+# Используя lambda, найдите максимальное из двух чисел.
+max_of_two = lambda x, y: x if x > y else y
+result = max_of_two(10, 15)
+print(result)
 
 # 3.task
+# Используя lambda, проверьте, является ли число чётным.
+is_even = lambda x: x % 2 == 0
+result = is_even(8)
+print(result)
 
 # 4.task
+# Используя lambda, соедините два слова в одну строку.
+concat = lambda x, y: x + " " + y
+result = concat("Hello", "World")
+print(result)
 
 # 5.task
